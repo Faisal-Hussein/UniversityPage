36e2d8310710fadac4acd9bbd1d47ea0009cc462
Faisal-Hussein <fh069017@ohio.edu>
2025-02-24 14:10:17 -0500

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
@@ -10,8 +11,10 @@ const App = () => {
       <Navbar />
       <Hero/>
       <div className="container">
-        <Title/>
+        <Title subTitle='Our Program' title='What We Offer'/>
         <Programs/>
+        <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
       </div>
     </div>
   )
