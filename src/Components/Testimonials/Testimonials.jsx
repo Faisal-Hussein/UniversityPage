4b6cc45c91b2b5ef2f91d39116b3d6d9e394e15d
Faisal-Hussein <fh069017@ohio.edu>
2025-03-16 13:04:22 -0400

    updated

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -49,7 +49,23 @@ const Testimonials = () => {
         <li>
           <div className="slide">
             <div className="user-info">
-              <img src={user_2} alt="" />
+              <img src={user_3} alt="" />
+              <div>
+                <h3>William Jackson</h3>
+                <span>Edusity, USA</span>
+              </div>
+            </div>
+            <p>Choosing to pursue my degree at Edusity was one of 
+              the best decisions I've ever made. The supportive community, state-of-the-art facilities,
+              and committment to academic excellence have truly exceeded
+              my expectations.
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className="slide">
+            <div className="user-info">
+              <img src={user_4} alt="" />
               <div>
                 <h3>William Jackson</h3>
                 <span>Edusity, USA</span>
