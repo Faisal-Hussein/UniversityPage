260973c3d03dc12cb75421625c6682da1ca1fa61
Faisal-Hussein <fh069017@ohio.edu>
2025-03-17 00:46:41 -0400

    updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Contact.css'
 import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
-import msg_icon from '../../assets/msg-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
 import msg_icon from '../../assets/msg-icon.png'
 
 const Contact = () => {
