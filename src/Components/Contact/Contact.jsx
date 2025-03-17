4d40900aeab0bfd3b42f174a60612f23a3360160
Faisal-Hussein <fh069017@ohio.edu>
2025-03-17 00:31:35 -0400

    updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,7 +4,14 @@ import './Contact.css'
 const Contact = () => {
   return (
     <div className='contact'>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <h3>Send us a message</h3>
+        <p>Feel free to reach out through our contact form or find our
+          contact information below. Your feedback, questions, and suggestions
+          are important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+      </div>
       <div className="contact-col"></div>
     </div>
   )
