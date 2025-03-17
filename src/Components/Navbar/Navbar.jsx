0e64dc9a61240d0a4b293cf6ef6a3bd56075b90d
Faisal-Hussein <fh069017@ohio.edu>
2025-03-17 03:27:03 -0400

    Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <img src={logo} alt="" className='logo'/>
         <ul>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to='program' smooth={true} offset={0} duration={500}>Program</Link></li>
+            <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={0} duration={500}>About us</Link></li>
             <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
             <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
