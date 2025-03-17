b8c504ab2a7de15d55ca88211c7bd396b1c8d3ac
Faisal-Hussein <fh069017@ohio.edu>
2025-03-17 03:27:36 -0400

    Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,8 +21,8 @@ const Navbar = () => {
             <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
             <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
-            <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
-            <li><Link to='contact' smooth={true} offset={0} duration={500}>Contact us</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500}>Contact us</Link></li>
         </ul>
     </nav>
   )
